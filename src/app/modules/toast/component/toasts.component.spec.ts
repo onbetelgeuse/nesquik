ed23f43cef237f4a66a58e87c2af4db7ebd58653
Jean-Michel FRECINAT <onbetelgeuse@gmail.com>
2019-09-25 20:25:11 +0200

    Integrate Jest to Angular

--- src/app/modules/toast/component/toasts.component.spec.ts
+++ src/app/modules/toast/component/toasts.component.spec.ts
@@ -1,22 +1,16 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ToastsComponent } from './toasts.component';
+import { ToastService } from '../service/toast.service';
+import { mock, instance } from 'ts-mockito';
 
 describe('ToastsComponent', () => {
   let component: ToastsComponent;
-  let fixture: ComponentFixture<ToastsComponent>;
-
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ ToastsComponent ]
-    })
-    .compileComponents();
-  }));
+  let toastService: ToastService;
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(ToastsComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+    toastService = mock(ToastService);
+    component = new ToastsComponent(instance(toastService));
   });
 
   it('should create', () => {
