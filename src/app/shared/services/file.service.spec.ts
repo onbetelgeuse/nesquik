f0b4dfe0513174c84c92bc13c0c7e0e87fcc19f7
Jean-Michel FRECINAT <onbetelgeuse@gmail.com>
2019-10-03 22:15:27 +0200

    fix download

--- src/app/shared/services/file.service.spec.ts
+++ src/app/shared/services/file.service.spec.ts
@@ -1,12 +1,18 @@
 import { TestBed } from '@angular/core/testing';
 
 import { FileService } from './file.service';
+import { HttpClient } from '@angular/common/http';
+import { mock, instance } from 'ts-mockito';
 
 describe('FileService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  let service: FileService;
+  let http: HttpClient;
+  beforeEach(() => {
+    http = mock(HttpClient);
+    service = new FileService(instance(http));
+  });
 
   it('should be created', () => {
-    const service: FileService = TestBed.get(FileService);
     expect(service).toBeTruthy();
   });
 });
--- src/app/shared/services/upload.service.spec.ts
+++ src/app/shared/services/upload.service.spec.ts
@@ -1,12 +1,16 @@
-import { TestBed } from '@angular/core/testing';
-
 import { UploadService } from './upload.service';
+import { HttpClient } from '@angular/common/http';
+import { mock, instance } from 'ts-mockito';
 
 describe('UploadService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  let service: UploadService;
+  let http: HttpClient;
+  beforeEach(() => {
+    http = mock(HttpClient);
+    service = new UploadService(instance(http));
+  });
 
   it('should be created', () => {
-    const service: UploadService = TestBed.get(UploadService);
     expect(service).toBeTruthy();
   });
 });
