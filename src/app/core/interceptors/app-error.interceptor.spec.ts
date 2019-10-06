48b6a319f3caa0bd046ca9475e95b1563122f358
Jean-Michel FRECINAT <onbetelgeuse@gmail.com>
2019-10-06 22:40:32 +0200

    fix test

--- src/app/core/interceptors/app-error.interceptor.spec.ts
+++ src/app/core/interceptors/app-error.interceptor.spec.ts
@@ -1,7 +1,15 @@
 import { AppErrorInterceptor } from './app-error.interceptor';
+import { AuthService } from '../../shared/services/auth.service';
+import { mock, instance } from 'ts-mockito';
 
 describe('AppError', () => {
+  let interceptor: AppErrorInterceptor;
+  let authService: AuthService;
+  beforeEach(() => {
+    authService = mock(AuthService);
+    interceptor = new AppErrorInterceptor(instance(authService));
+  });
   it('should create an instance', () => {
-    expect(new AppErrorInterceptor()).toBeTruthy();
+    expect(interceptor).toBeTruthy();
   });
 });
