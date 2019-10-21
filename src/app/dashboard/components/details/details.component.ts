51018b86217c89927dadb12f6e931e5a2c524a5e
Jean-Michel FRECINAT <onbetelgeuse@gmail.com>
2019-10-21 07:29:05 +0200

    formarray validator

--- src/app/dashboard/components/details/details.component.ts
+++ src/app/dashboard/components/details/details.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators, FormArray } from '@angular/forms';
 import { FileService } from 'src/app/shared/services/file.service';
 import { StructureValidators } from 'src/app/shared/validators/structure.validators';
 import { FileDto } from 'src/app/shared/models/file.model';
@@ -13,6 +13,11 @@ import { FileDto } from 'src/app/shared/models/file.model';
 export class DetailsComponent implements OnInit {
   public form: FormGroup;
   @Input() file: FileDto;
+
+  public get images(): FormArray {
+    return this.form.controls.images as FormArray;
+  }
+
   constructor(
     public activeModal: NgbActiveModal,
     public readonly fileService: FileService,
@@ -47,6 +52,16 @@ export class DetailsComponent implements OnInit {
           ),
         ],
       ],
+      images: this.fb.array([], [], [StructureValidators.uniqueImage]),
     });
+  }
+
+  public addImageFormGroup(): void {
+    const newImageFormGroup: FormGroup = this.createImageGroup();
+    this.images.push(newImageFormGroup);
+  }
+
+  private createImageGroup(): FormGroup {
+    return this.fb.group({ name: ['', []], filename: ['', []] });
   }
 }
--- src/app/shared/validators/structure.validators.ts
+++ src/app/shared/validators/structure.validators.ts
@@ -1,4 +1,10 @@
-import { ValidationErrors, AbstractControl, ValidatorFn } from '@angular/forms';
+import {
+  ValidationErrors,
+  AbstractControl,
+  ValidatorFn,
+  FormArray,
+  FormGroup,
+} from '@angular/forms';
 import { timer, of } from 'rxjs';
 import { FileService } from '../services/file.service';
 import { map, switchMap } from 'rxjs/operators';
@@ -9,11 +15,11 @@ export class StructureValidators {
     fileService: FileService,
     previous: string = null,
     dueTime: number = 250,
-  ): ValidatorFn {
-    return (input: AbstractControl): ValidationErrors | null => {
+  ): ValidatorFn | any {
+    return (control: AbstractControl): ValidationErrors | null => {
       return timer(dueTime).pipe(
         switchMap(() => {
-          const name: string = input.value;
+          const name: string = control.value;
           const isUniqueName: boolean = fileService.files
             .map((file: FileDto) => file.originalname)
             .filter((value: string) => value !== previous)
@@ -22,6 +28,33 @@ export class StructureValidators {
           return of(isUniqueName);
         }),
         map(isUniqueName => (!isUniqueName ? null : { unavailable: true })),
+      );
+    };
+  }
+
+  public static get uniqueImage(): ValidatorFn | any {
+    return (control: AbstractControl): ValidationErrors | null => {
+      return timer(500).pipe(
+        switchMap(() => {
+          if (!(control instanceof FormArray)) {
+            return of(false);
+          }
+
+          const controls: AbstractControl[] = control.controls.filter(
+            (formGroup: AbstractControl, index: number) =>
+              control.controls.findIndex(
+                (searchFormGroup: AbstractControl) =>
+                  formGroup.get('name').value ===
+                    searchFormGroup.get('name').value &&
+                  formGroup.get('filename').value ===
+                    searchFormGroup.get('filename').value,
+              ) !== index,
+          );
+
+          console.log(controls);
+
+          return of(false);
+        }),
       );
     };
   }
